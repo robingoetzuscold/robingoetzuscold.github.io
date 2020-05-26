b921151844400301cc4a7c3eabdb95294989659d
Robin Goetz <tug29225@temple.edu>
2020-05-26 13:08:24 -0400

    05/26 dark mode is coming

---
+++ component---src-pages-contact-thanks-tsx-4f066caf2fd0ea1a6a43.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{"j/O4":function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),s=n.n(t),c=n("GWJV");a.default=function(){return s.a.createElement(c.a,null,s.a.createElement("section",{className:"section"},s.a.createElement("div",{className:"container"},s.a.createElement("div",{className:"content"},s.a.createElement("h1",null,"Thank you!"),s.a.createElement("p",null,"This is a custom thank you page for form submissions")))))}}}]);
+//# sourceMappingURL=component---src-pages-contact-thanks-tsx-4f066caf2fd0ea1a6a43.js.map
